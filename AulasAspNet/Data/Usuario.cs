4e178e66b0be7b07cd4b82fbab042538b0bf1e39
1717373 <lucasdouglas-10@hotmail.com>
2018-10-04 20:33:08 -0300

    Aula01-Parte1

---
+++ AulasAspNet/Data/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AulasAspNet.Data
+{
+    public class Usuario
+    {
+        public int UsuarioId { get; set; }
+
+        public String Nome { get; set; }
+
+        public String Senha { get; set; }
+
+
+    }
+}
