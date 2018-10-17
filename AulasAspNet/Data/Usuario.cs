0063a07477d714b01925664690e62e316196566d
1717373 <lucasdouglas-10@hotmail.com>
2018-10-16 21:40:49 -0300

    alterações dia 16/10

--- AulasAspNet/Data/Usuario.cs
+++ AulasAspNet/Data/Usuario.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,16 +9,22 @@ namespace AulasAspNet.Data
     public class Usuario
     {
         public int UsuarioId { get; set; }
-
+        [Required(ErrorMessage ="Nome é obrigatório")]
+        [MaxLength(30, ErrorMessage ="No maximo 30 caracteres")]
         public string Nome { get; set; }
-
+      
         public string Senha { get; set; }
 
         public string CPF { get; set; }
-
+        
+        [Required(ErrorMessage = "E-mail é obrigatório")]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "Data de nascimento é obrigatório")]
         public string Nascimento { get; set; }
+
+        [RegularExpression(@"\d\d\d\d\d-\d\d\d", ErrorMessage = "O Cep deve conter somente oito digitos válidos")]
+        public string Cep { get; set; }
 
         public string Logradouro { get; set; }
 
